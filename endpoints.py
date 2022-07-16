ab6ac4c5e1ff0b03e9f9246c463a72d9ab911aab
patrykkondrat <01150244@pw.edu.pl>
2022-07-16 23:15:22 +0200

    fix of issue in app url where miss slash at begin

--- endpoints.py
+++ endpoints.py
@@ -1,35 +1,46 @@
 from fastapi import APIRouter, Response, status
 from dependency_injector.wiring import inject
-from services import NotFoundId, Service, NotFoundId
+from services import IdNotFoundInDatabase, Service
 
 
 router = APIRouter()
 
-Service = Service()
+service = Service()
 
 @router.get("/get/all")
 @inject
 async def get_them_all():
-    return Service.get_links()
+    return service.get_links()
 
 
 @router.get("/get/{id}")
 @inject
 async def get_by_id(id: int):
-    return Service.get_link_by_id(id)
+    try:
+        return service.get_link_by_id(id)
+    except IdNotFoundInDatabase:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
 
 
 @router.post("post/{id}/{long_link}")
 @inject
-async def do_something(id, long_link):
-    return Service.create_link(id,long_link)
+async def do_something(id: int, long_link: str):
+    return service.create_link(id, long_link)
 
 
 @router.delete("del/{id}")
 @inject
-async def remove(id):
-    Service.delete_link_by_id(id)
+async def remove(id: int):
+    try:
+        service.delete_link_by_id(id)
+    except IdNotFoundInDatabase:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+    else:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.get("/status")
-async def status():
+@inject
+async def _status():
     return {"status": "GIT"}
+
+
